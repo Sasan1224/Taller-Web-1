781bb34ede363cca8963bd52f135fc5eb7bfa13a
MYSSYF <jogabolu@gmail.com>
2022-04-19 21:15:58 -0500

    se intento

---
+++ myshopu/app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYr3FXuwK2rydv5Y0A-5h-5HhXzxz-d_0",
+  authDomain: "le-tiende.firebaseapp.com",
+  projectId: "le-tiende",
+  storageBucket: "le-tiende.appspot.com",
+  messagingSenderId: "815737153738",
+  appId: "1:815737153738:web:a791268871eecfe5de6c33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
